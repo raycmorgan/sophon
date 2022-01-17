d6c0e77e6df4f4ee15ca0c02a37542bec10c6b95
Ray Morgan <ray@ray.codes>
2022-01-17 12:04:58 -0800

    Remove unused features

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-#![feature(int_log)]
-#![feature(slice_as_chunks)]
 #![feature(int_abs_diff)]
 
 #![allow(incomplete_features)]
-#![feature(generic_const_exprs)]
 #![feature(new_uninit)]
 #![feature(allocator_api)]
 
